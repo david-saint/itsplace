2a7208c0c74ae0f71064ded2d90b1556e8640b15
faye12005@gmail.com <faye12005@gmail.com@6f1d606a-7dde-e55d-4749-17cd9a74cd1b>
2012-04-10 08:49:33 +0000

    config delete

--- itsplace/src/main/java/com/itsplace/admin/AdminController.java
+++ itsplace/src/main/java/com/itsplace/admin/AdminController.java
@@ -19,85 +19,82 @@ import com.myplace.user.User;
 import com.myplace.user.UserService;
 import com.myplace.util.PagingManager;
 
-
-
 /**
- * 웹 관리자 기능   
+ * 웹 관리자 기능
  * */
 
 @Controller
 public class AdminController {
-        
-	private static final Logger logger =  LoggerFactory.getLogger(FranchiserController.class);
+
+	private static final Logger logger = LoggerFactory
+			.getLogger(FranchiserController.class);
 	@Autowired
 	private FranchiserService fService;
-	
+
 	@Autowired
 	private AdminService aService;
-	
+
 	@Autowired
 	private UserService uService;
-	
+
 	private PagingManager pagingManaer;
-	
+
 	@Autowired
 	private CommonService commonService;
-	
+
 	/**
 	 * <pre>
-	 *	가맹점 신청 승인: 승인이 완료되면 가맹점 전용 로그인 아이디(fid) 계정이 생성된다				
+	 * 가맹점 신청 승인: 승인이 완료되면 가맹점 전용 로그인 아이디(fid) 계정이 생성된다
 	 * </pre>
+	 * 
 	 * @author 김동훈
 	 * @version 1.0, 2011. 9. 29.
-	 * @param 
-	 * @return  가맹점 신청(가입) 페이지
-	 * @throws 
-	 * @see 
+	 * @param
+	 * @return 가맹점 신청(가입) 페이지
+	 * @throws
+	 * @see
 	 */
 	@RequestMapping(value = "/admin/auth/{fid}/{authyn}", method = RequestMethod.GET)
- 	public @ResponseBody String auth(
- 			 @PathVariable int fid,
- 			 @PathVariable String authyn) {
-		
-		
-		logger.info("프랜차이즈 FID:" +fid +" 승인여부:"+ authyn);
+	public @ResponseBody
+	String auth(@PathVariable int fid, @PathVariable String authyn) {
+
+		logger.info("프랜차이즈 FID:" + fid + " 승인여부:" + authyn);
 		FranchiserMember franchiserMember = fService.getFranchiserMember(fid);
-		
+
 		franchiserMember.setAuthyn(authyn); // Y/N
-		
+
 		aService.updateFranchiserAuth(franchiserMember);
-		
-		if(authyn.equals("Y")){			
+
+		if (authyn.equals("Y")) {
 			User user = new User();
 			user.setEmail(Integer.toString(fid));
 			user.setPassword(Integer.toString(fid));
 			user.setName(franchiserMember.getFname());
 			user.setRole("ROLE_FID");
-			
-			
-			if((User)uService.getUser(Integer.toString(fid))==null  ){
-				uService.saveUser(user);		
-			}else{
+
+			if ((User) uService.getUser(Integer.toString(fid)) == null) {
+				uService.saveUser(user);
+			} else {
 				uService.updateUserEnable(user);
-				logger.info("가맹점 신청 승인이 되고 가맹점 아이디(fid):" + fid +" 가 PUSER 에 이미 등록되어 있습니다");				
-			}			
+				logger.info("가맹점 신청 승인이 되고 가맹점 아이디(fid):" + fid
+						+ " 가 PUSER 에 이미 등록되어 있습니다");
+			}
 			logger.info("가맹점 신청 승인 완료");
 			return "authSuccess";
-		}else{
-			logger.info("가맹점 신청 승인 취소");			
+		} else {
+			logger.info("가맹점 신청 승인 취소");
 			User user = new User();
-			user.setEmail(Integer.toString(fid));			
+			user.setEmail(Integer.toString(fid));
 			uService.updateUserDisable(user);
 			return "authInvoke";
 		}
-		
-		
+
 	}
+
 	@RequestMapping(value = "/administrator", method = RequestMethod.GET)
 	public String administrator() {
-	
+
 		return "admin/main";
 	}
-	
-	
+
 }
